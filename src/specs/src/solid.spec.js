f0d0266666a143961388b8e0a9d6e27b6747980c
Darkatom <darkatomish@gmail.com>
2016-10-18 18:08:46 +0200

    solid_specs

--- src/specs/src/solid.spec.js
+++ src/specs/src/solid.spec.js
@@ -5,71 +5,72 @@ var assert = require('assert');
 
 console.log("- Testing Solid -");
 describe('Solid', function() {
-  describe('#collision()', function() {
-    it('Should return "top" when the ball collides the top', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
-      ball.x = solid.x + solid.w/2;
-      ball.y = solid.y - ball.radius;
+  describe('#collision(ball)', function() {
+    var solid = new Solid(100, 100, 20, 20);
+    var ball = new Ball(0, 0);
+
+    it('Ball collides the top', function() {
+      ball.setPosition(solid.position.x + solid.width/2, solid.position.y);
+      ball.setLastPosition(ball.position.x, ball.position.y - solid.height/2);
+
       assert.equal("top", solid.collision(ball));
     });
 
-    it('Should return "bottom" when the ball collides the bottom', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
-      ball.x = solid.x + solid.w/2;
-      ball.y = solid.y + solid.h + ball.radius;
+    it('Ball collides the bottom', function() {
+      ball.setPosition(solid.position.x + solid.width/2, solid.position.y + solid.height);
+      ball.setLastPosition(ball.position.x, ball.position.y + solid.height/2);
+
       assert.equal("bottom", solid.collision(ball));
     });
     
-    it('Should return "left" when the ball collides the left', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
-      ball.x = solid.x - ball.radius;
-      ball.y = solid.y + solid.h/2;
+    it('Ball collides the left', function() {
+      ball.setPosition(solid.position.x, solid.position.y + solid.height/2);
+      ball.setLastPosition(ball.position.x - solid.width/2, ball.position.y);
+
       assert.equal("left", solid.collision(ball));
     });
     
-    it('Should return "right" when the ball collides the right', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
-      ball.x = solid.x + solid.w + ball.radius;
-      ball.y = solid.y + solid.h/2;
+    it('Ball collides the right', function() {
+      ball.setPosition(solid.position.x + solid.width, solid.position.y + solid.height/2);
+      ball.setLastPosition(ball.position.x + solid.width/2, ball.position.y);
       assert.equal("right", solid.collision(ball));
     });
     
-    it('Should return null when the ball does not collide', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
+    it('Ball does not collide', function() {
+      ball.setPosition(0, 0);
+      ball.setLastPosition(0, 0);
       assert.equal(null, solid.collision(ball));
     });
   });
 
-  describe('#collided()', function() {
-    it('Passed "top" value, the new ball position should be on top of the solid', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
+  describe('#collided(), checked with #collision(ball)', function() {
+    var solid = new Solid(100, 100, 20, 20);
+    var ball = new Ball(0, 0);
+
+    it("Ball is repositioned at solid's top, and collides", function() {
+      ball.setPosition(solid.position.x + solid.width/2, solid.position.y);
+      ball.setLastPosition(ball.position.x, ball.position.y - solid.height/2);
       solid.collided("top", ball);
       assert.equal("top", solid.collision(ball));
     });
     
-    it('Passed "bottom" value, the new ball position should be beneath of the solid', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
+    it("Ball is repositioned at solid's bottom, and collides", function() {
+      ball.setPosition(solid.position.x + solid.width/2, solid.position.y + solid.height);
+      ball.setLastPosition(ball.position.x, ball.position.y + solid.height/2);
       solid.collided("bottom", ball);
       assert.equal("bottom", solid.collision(ball));
     });
     
-    it('Passed "left" value, the new ball position should be to the left of the solid', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
+    it("Ball is repositioned at solid's left, and collides", function() {
+      ball.setPosition(solid.position.x, solid.position.y + solid.height/2);
+      ball.setLastPosition(ball.position.x - solid.width/2, ball.position.y);
       solid.collided("left", ball);
       assert.equal("left", solid.collision(ball));
     });
     
-    it('Passed "right" value, the new ball position should be to the right of the solid', function() {
-      var solid = new Solid(100, 100, 20, 20);
-      var ball = new Ball(0, 0);
+    it("Ball is repositioned at solid's right, and collides", function() {
+      ball.setPosition(solid.position.x + solid.width, solid.position.y + solid.height/2);
+      ball.setLastPosition(ball.position.x + solid.width/2, ball.position.y);
       solid.collided("right", ball);
       assert.equal("right", solid.collision(ball));
     });
